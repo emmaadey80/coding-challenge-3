d5c5c51aa23f6d3ee24149f77df07c496b7e4ebb
Emma Adey <eadey@usf.edu>
2025-01-28 13:16:38 -0500

    Task 1- Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,16 @@
+// Task 1: Product Price Management Scenario
+console.log("Task 1- Product Price Management");
+
+// Declare an array 'prices'
+let prices = [25, 63, 42, 99, 53];
+console.log("Inital Prices:", prices);
+
+// Adding a new price to the array (12)
+prices.push(12);
+
+// Removing the first price from the array (25)
+prices.shift();
+
+// Logging the updated price list to console (63, 42, 99, 53, 12)
+console.log("Updated Prices:", prices);
+
